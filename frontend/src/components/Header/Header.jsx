db544b98ee50a309671530b2e23ce121b0782b0a
Harshita Phadtare <hphadtare2023@gmail.com>
2024-11-18 02:57:21 +0530

    header section half complete

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your delicious healthy food here!</h2>
+        <p>Transform your fitness journey with personalized plans, expert guidance, and a supportive community. Whether you’re aiming for strength, flexibility, or endurance, we’ve got you covered. Start your path to a healthier, fitter you today!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
