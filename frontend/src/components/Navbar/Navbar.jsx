41c59496a4d6f204da3691748e5c4d40450e3d2c
Harshita Phadtare <hphadtare2023@gmail.com>
2024-11-18 02:19:37 +0530

    navbar component complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 import { assets } from '../../assets/frontend_assets/assets';
 
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("home");
+
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="feastio-logo" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile app</li>
-        <li>contact us</li>
+        <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search-icon" />
@@ -19,10 +22,24 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="basket-icon" />
             <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        {/* signup button */}
+        <div class="container">
+          <div class="wrapper">
+              <button class="btn">
+                  <div class="btn__bg">
+                      <span class="btn__bg__layer btn__bg__layer-first"></span>
+                      <span class="btn__bg__layer btn__bg__layer-second"></span>
+                      <span class="btn__bg__layer btn__bg__layer-third"></span>
+                  </div>
+                  <span class="btn__text-out">sign in</span>
+                  <span class="btn__text-in">sign in</span>
+              </button>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 export default Navbar;
+
