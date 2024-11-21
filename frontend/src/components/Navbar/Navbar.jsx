dd214df24580a47b27a9ffac1888a95896a6da89
Harshita Phadtare <hphadtare2023@gmail.com>
2024-11-22 01:24:36 +0530

    login and signup added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import "./Navbar.css";
 import { assets } from '../../assets/frontend_assets/assets';
+import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu] = useState("home");
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="feastio-logo" className="logo" />
+      <img src={assets.logo} alt="nutrifit-logo" className="logo" />
       <ul className="navbar-menu">
-        <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile app</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</li>
+        <Link to="/" onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile app</a>
+        <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search-icon" />
@@ -25,7 +26,7 @@ const Navbar = () => {
         {/* signup button */}
         <div className="container">
           <div className="wrapper">
-              <button className="btn">
+              <button className="btn" onClick={()=>setShowLogin(true)}>
                   <div className="btn__bg">
                       <span className="btn__bg__layer btn__bg__layer-first"></span>
                       <span className="btn__bg__layer btn__bg__layer-second"></span>
