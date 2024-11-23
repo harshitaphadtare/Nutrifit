ba1a86a87c62d65864bbb5a648bcffd1ee3e695a
Harshita Phadtare <hphadtare2023@gmail.com>
2024-11-23 20:36:03 +0530

    started with placeorder page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,9 +3,18 @@ import "./PlaceOrder.css";
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
-    </div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
+        </div>
+      </div>  
+      <div className="place-order-right">
+      {/* start */}
+      </div>
+    </form>
   )
 }
 
