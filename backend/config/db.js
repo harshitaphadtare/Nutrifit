dbbc5c8c0f7c39e6e042095d95c464db81035961
Harshita Phadtare <hphadtare2023@gmail.com>
2024-11-24 21:40:47 +0530

    db changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async() =>{
-    await mongoose.connect("mongodb+srv://harshita:hphadtare2024@cluster0.hiszs.mongodb.net/food-del").then(()=>console.log("DB connected"));
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(()=>console.log("DB connected"));
 }
 
