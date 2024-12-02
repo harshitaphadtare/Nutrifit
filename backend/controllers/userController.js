72a80b48fcc2a39a0f90f82a05db3a8fad56b2cf
Harshita Phadtare <hphadtare2023@gmail.com>
2024-12-03 01:26:22 +0530

    login function added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,9 +3,28 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 
-
+//login user function
 const loginUser = async (req, res) => {
-  
+  const {email,password} = req.body;
+  try {
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        return res.json({success:false,message:"User doesn't exit!"});
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+
+    if(!isMatch){
+        return res.json({success:false,message:"Invalid credentials!"});
+    }
+
+    const token = createToken(user._id);
+    res.json({success:true,token});
+
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"});
+  }
 };
 
 
