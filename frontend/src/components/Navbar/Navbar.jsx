15f92282fbaba4e77610362635cdfa465996eb90
Harshita Phadtare <hphadtare2023@gmail.com>
2024-12-03 15:26:07 +0530

    login functionality added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import "./Navbar.css";
 import { assets } from '../../assets/frontend_assets/assets';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 
@@ -9,7 +9,15 @@ const Navbar = ({setShowLogin}) => {
 
   const [menu,setMenu] = useState("home");
 
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
+
+  const navigate = useNavigate();
+
+  const logout = () =>{
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  }
 
   return (
     <div className='navbar'>
@@ -26,8 +34,7 @@ const Navbar = ({setShowLogin}) => {
             <Link to="/cart"><img src={assets.basket_icon} alt="basket-icon" /></Link>
             <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        {/* signup button */}
-        <div className="container">
+        {!token?<div className="container">
           <div className="wrapper">
               <button className="btn" onClick={()=>setShowLogin(true)}>
                   <div className="btn__bg">
@@ -39,7 +46,16 @@ const Navbar = ({setShowLogin}) => {
                   <span className="btn__text-in">sign in</span>
               </button>
           </div>
-        </div>
+        </div>:<div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className='nav-profile-dropdown'>
+            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr />
+            <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+          </ul>
+        </div>}
+  
+        
       </div>
     </div>
   )
