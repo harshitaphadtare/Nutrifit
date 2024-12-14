28f2caa241e59b6b2573f91a5c24f29bb73ef4b6
Harshita Phadtare <hphadtare2023@gmail.com>
2024-12-14 17:04:19 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing use order from frontend
 const placeOrder = async(req,res)=>{
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://nutrifit-frontend-4mau.onrender.com/";
     try {
         const newOrder = new orderModel({
             userId:req.body.userId,
